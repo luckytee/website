c834cdcf9dda7a5987176dc49335da75d0059df4
terramae <terramae@umich.edu>
2023-07-20 16:52:40 -0400

    ghost

--- js/lissajous.js
+++ js/lissajous.js
@@ -1,29 +1,37 @@
+let ghost;
+
+function preload() {
+  ghost = loadImage('images/ghost.png');
+}
+
 function setup() {
-    frameRate(10);
-    createCanvas(400, 400);
+  frameRate(10);
+  createCanvas(400, 400);
+}
+
+let x = 50;
+let y = 50;
+let t = 0;
+
+function draw() {
+  background(0);
+
+  fill(102, 204, 0, 50);
+  noStroke();
+
+  for (i = 0; i < 400; i++) {
+
+    x = 160 * sin(1 * t + PI / 2);
+    y = 160 * sin(3 * t);
+
+    push();
+    translate(width / 2 + y, height / 2 + x);
+    // square(-15, -15, 30);
+    ghost.resize(30, 30);
+    image(ghost, -15, -15);
+    pop();
+
+    // t+=17;
+    t += mouseX / 10;
   }
-  
-  let x = 50;
-  let y = 50;
-  let t = 0;
-  
-  function draw() {
-    background(0);
-    
-    fill(102, 204, 0, 50);
-    noStroke();
-    
-    for (i = 0; i < 400; i++) {
-  
-        x = 160*sin(1*t+PI/2);
-        y = 160*sin(3*t);
-      
-      push();
-      translate(width/2 + y, height/2 + x);
-      square(-15, -15, 30);
-      pop();
-      
-        // t+=17;
-      t += mouseX/10;
-    }
-  }
+}
